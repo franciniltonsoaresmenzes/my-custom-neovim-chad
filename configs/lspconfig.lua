1b505bf10a3018acee74ba983e40a5fb8d786261
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-11-04 19:51:03 -0300

    lsp

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "eslint", "emmet_ls", "tailwindcss", "prismals", "docker_compose_language_service" }
+local servers = { "html", "cssls", "tsserver", "clangd", "eslint", "emmet_ls", "tailwindcss", "prismals", "docker_compose_language_service", "pyright", "java_language_server" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
